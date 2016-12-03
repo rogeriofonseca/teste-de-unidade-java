a5921eeb215e4cf3c1841a70548b757bdc3bc500
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-12-03 02:04:21 -0200

    Removendo target enviado acidentalmente

---
+++ src/test/java/br/com/tdd/app/AvaliadorTest.java
@@ -0,0 +1,34 @@
+package br.com.tdd.app;
+
+import br.com.caelum.leilao.dominio.Avaliador;
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import junit.framework.TestCase;
+
+public class AvaliadorTest extends TestCase {
+    
+    public void testMaiorLance(){
+       // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(maria,250.0));
+        leilao.propoe(new Lance(joao,300.0));
+        leilao.propoe(new Lance(jose,400.0)); 
+
+        // Executando a acao do Avaliador
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        // compara a saida com o esperado
+        double maiorEsperado = 400;
+        double menorEsperado = 250;
+
+        assertEquals(maiorEsperado, leiloeiro.getMaiorLance());
+        assertEquals(menorEsperado, leiloeiro.getMenorLance());
+    }
+}
